8185e4b39045b72d34f0e23523551f98e452f43f
Brian Engen <engenb@gmail.com>
2016-05-07 16:35:15 -0500

    fixing project

---
+++ autofac/Autofac101/Autofac101/Services/ComputeFibonacci/FibonacciSequenceComputer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Autofac101.Services.ComputeFibonacci
+{
+	public class FibonacciSequenceComputer : IComputeFibonacciSequence
+	{
+		private int Count { get; }
+
+		private IComputeFibonacciNumber FibonacciNumber { get; }
+
+		public FibonacciSequenceComputer(int count, IComputeFibonacciNumber fibonacciNumber)
+		{
+			Count = count;
+			FibonacciNumber = fibonacciNumber;
+		}
+
+		public void Compute()
+		{
+			var sequence = Enumerable.Range(0, Count)
+				.Select(FibonacciNumber.Compute)
+				.ToArray();
+
+			Console.WriteLine($"The first {Count} numbers in the Fibonacci sequence are:");
+			Console.WriteLine(string.Join(" ", sequence));
+		}
+	}
+}
---
+++ autofac/Autofac101/Autofac101/Services/ComputeFibonacci/IComputeFibonacciNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autofac101.Services.ComputeFibonacci
+{
+	public interface IComputeFibonacciNumber
+	{
+		int Compute(int n);
+	}
+}
